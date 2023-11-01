851b8199d1d300d1fc7f2c8f02cd7dc6c0f2442d
Antoine Jay <ajay@lillou>
2023-11-01 09:06:07 +0100

    Wait entering the directory to change path

--- examples/Evolution_a-Si.LAMMPS.d/KMC.sh
+++ examples/Evolution_a-Si.LAMMPS.d/KMC.sh
@@ -17,8 +17,8 @@ for istep in `seq 1 $nsteps`; do
        echo  Running events searches with group $igroup containing $nparf cores
        mkdir events_group_$igroup
        cp lammps.in conf.sw Si.sw artn.in events_group_$igroup 
-       sed -i "s| ..\/..\/Files_LAMMPS\/libartn-lmp.so| ..\/..\/..\/Files_LAMMPS\/libartn-lmp.so|g" lammps.in #put the correct path in lammps.in  
        cd events_group_$igroup
+       sed -i "s| ..\/..\/Files_LAMMPS\/libartn-lmp.so | ..\/..\/..\/Files_LAMMPS\/libartn-lmp.so |g" lammps.in #put the correct path in lammps.in  
        sed -i -e '21d' artn.in
        sed -i -e "21i\ push_ids= $((1 + $RANDOM % 1000)) "  artn.in    # Modify ARTn parameters if needed, here the central atom for the event
        mpirun -np $nparf $LAMMPS_PATH/src/lmp_mpi -in lammps.in >>artn.log &  # The & permits to place all the processes in background
