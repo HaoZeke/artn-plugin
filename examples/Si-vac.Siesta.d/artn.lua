599906d48e47f3236e7bce1d17fd88933c1f4b73
mgoonde <miha.gunde@irb.hr>
2024-01-29 14:43:25 +0100

    update artn.lua script

--- examples/Si-vac.Siesta.d/artn.lua
+++ examples/Si-vac.Siesta.d/artn.lua
@@ -1,5 +1,4 @@
--- This is a lua script which calls pARTn
-
+-- package.cpath = package.cpath .. ";/home/mgunde/artn_siesta/Files_Siesta/partn_lua.so"
 
 local flos = require "flos"
 local Unit = siesta.Units
@@ -18,6 +17,7 @@ local f_alpha = 0.99
 local N_min = 5
 
 
+-- init FIRE params
 local FIRE = {}
 FIRE = flos.FIRE{dt_init = dt_init,
                  alpha_init = alpha_init,
@@ -29,11 +29,14 @@ FIRE = flos.FIRE{dt_init = dt_init,
 
 FIRE.max_dF = 20.0 -- Bohr
 FIRE.tolerance = 1.0e-1 -- Force tolerance for optimization: if norm():max() is below this, algo exists. units: Ry/bohr
+
+
+
 -- This is the main function called from Siesta.
 function siesta_comm()
 
   -- Create new return table;
-  -- this will contain just keys of which elements are returned to siesta
+  -- this will contain keys of which elements are returned to siesta
   -- after the modifications to them are made.
   -- For now it's just empty.
   ret_tbl = {}
@@ -64,7 +67,7 @@ function siesta_comm()
     IOprint("\nLUA convergence information for the FIRE algorithms:")
 
     -- Ensure we update the convergence criteria
-    -- from SIESTA (in this way one can ensure siesta options)
+    -- from SIESTA
 
     -- FIRE.tolerance = siesta.MD.MaxForceTol * Unit.Ang / Unit.eV
     -- FIRE.max_dF = siesta.MD.MaxDispl-- / Unit.Ang
@@ -93,12 +96,8 @@ function siesta_comm()
                      "geom.fa",
                      "MD.Relaxed",
                      "E.total"})
-    print("iam in MOVE")
+    print("iam in siesta.MOVE")
 
-    -- write xyz of current conf before move
-    -- if siesta.Node == 0 then
-    --   print_xyz( siesta )
-    -- end
 
     -- call the function  which does things: input arg `siesta`
     -- contains table of properties
@@ -113,11 +112,6 @@ function siesta_comm()
     -- do a fire move
     -- ret_tbl = move_FIRE( siesta )
 
-
-    -- write xyz after move
-    -- if siesta.Node == 0 then
-      -- print_xyz( siesta )
-    -- end
 
     -- siesta.send({"Stop"})
   end
@@ -149,40 +143,50 @@ function move_artn( siesta )
   end
 
   -- to get eV/Ang forces, need to * Units.Ang / Units.eV
+
+  -- setup the needed variables
+
   -- number of atoms
   nat = siesta.geom.na_u
+
   -- current positions in Bohr
   tau = siesta.geom.xa
+
   -- current force in Ry/Bohr
   force = siesta.geom.fa
+
   -- lattice vectors in bohr, i think not scaled with alat?
   at = siesta.geom.cell
+
   -- energy in Ry
   etot = siesta.E.total
+
   -- atomic types
   ityp = siesta.geom.species
+
   -- order of atoms
   order = {}
+
   -- atomic coords fixed by the engine
   if_pos = flos.Array.empty(nat,3)
+
   -- current alpha value from FIRE
   alpha_curr = FIRE.alpha
+
   -- current value of dt from FIRE
   dt_curr = FIRE.dt
+
   -- n_P_pos from FIRE
   nsteppos = FIRE.n_P_pos
+
   -- current velocities from FIRE
   vel = FIRE.V
 
-  -- modified
+  -- modified force and velocity, to be returned from artn
   force_m = force
   vel_m = vel
 
 
-  if siesta.IONode then
-    print("vel is:")
-    print(vel)
-  end
   -- local variables
   local displ_vec = flos.Array.zeros(nat,3)
   local lrelax
@@ -209,9 +213,9 @@ function move_artn( siesta )
     if_pos[i][3] = 1
   end
   -- need to fix some atom for whatever reason: see comments in FIRE lua files
-  if_pos[38][1] = 0
-  if_pos[38][2] = 0
-  if_pos[38][3] = 0
+  -- if_pos[38][1] = 0
+  -- if_pos[38][2] = 0
+  -- if_pos[38][3] = 0
 
 
   -- call fortran: careful to the order of arguments/results
@@ -261,18 +265,16 @@ function move_artn( siesta )
   FIRE.alpha = alpha_curr_m
   FIRE.n_P_pos = nsteppos_m
   FIRE.V = flos.Array.from(vel_m)
-  -- siesta.geom.fa = ...
-  -- siesta.geom.fa = flos.Array.from(force_m)
 
-  if siesta.IONode then
-    print( "modif fa")
-    for i=1, nat do
-      -- print( i, siesta.geom.fa[i][1], siesta.geom.fa[i][2], siesta.geom.fa[i][3] )
-      print( i, force_m[i][1], force_m[i][2], force_m[i][3] )
-    end
-  end
+  -- if siesta.IONode then
+  --   print( "modif fa")
+  --   for i=1, nat do
+  --     -- print( i, siesta.geom.fa[i][1], siesta.geom.fa[i][2], siesta.geom.fa[i][3] )
+  --     print( i, force_m[i][1], force_m[i][2], force_m[i][3] )
+  --   end
+  -- end
 
--- control convergence from ARTn
+  -- control convergence by lconv retunred from ARTn
   siesta.MD.relaxed = lconv
 
   -- FIRE part
@@ -280,38 +282,47 @@ function move_artn( siesta )
     FIRE:info()
   end
 
+  -- create arrays for fire
   -- local xa = flos.Array.from(siesta.geom.xa)-- / Unit.Ang
   local xa = flos.Array.from( tau_m )
   local fa = flos.Array.from( force_m ) -- * Unit.Ang / Unit.eV
 
-  
-  if siesta.IONode then
-    print("fa")
-    for i=1, nat do
-      print( i, fa[i][1], fa[i][2], fa[i][3] )
+
+  -- if siesta.IONode then
+  --   print("fa")
+  --   for i=1, nat do
+  --     print( i, fa[i][1], fa[i][2], fa[i][3] )
+  --   end
+  --   print("fa:norm(0)", fa:norm(0) )
+  --   print("fa:norm()", fa:norm() )
+  -- end
+
+  -- if siesta.IONode then
+  --   print("xa[1][1]",xa[1][1])
+  --   print( "fa[48]", fa[48][1],fa[48][2],fa[48][3])
+  --   print("unit.Ang",Unit.Ang)
+  --   print("Unit.eV", Unit.eV)
+  --   print("lvonv",lconv)
+  -- end
+
+  if not lconv then
+    -- call fire step
+    local out_xa = FIRE:optimize( xa, fa)
+
+    local diff_xa = flos.Array.zeros(nat,3)
+    diff_xa = out_xa - xa
+    if siesta.IONode then
+      print( diff_xa )
     end
-    print("fa:norm(0)", fa:norm(0) )
-    print("fa:norm()", fa:norm() )
-  end
+    -- local relaxed = FIRE:optimized()
 
-  if siesta.IONode then
-    print("xa[1][1]",xa[1][1])
-    print( "fa[48]", fa[48][1],fa[48][2],fa[48][3])
-    print("unit.Ang",Unit.Ang)
-    print("Unit.eV", Unit.eV)
-  end
-  -- call fire step
-  local out_xa = FIRE:optimize( xa, fa)
+    siesta.geom.xa = out_xa-- * Unit.Ang
+    -- siesta.MD.Relaxed = relaxed
 
-  local diff_xa = flos.Array.zeros(nat,3)
-  diff_xa = out_xa - xa
-  if siesta.IONode then
-    print( diff_xa )
-  end
-  -- local relaxed = FIRE:optimized()
-
-  siesta.geom.xa = out_xa-- * Unit.Ang
-  -- siesta.MD.Relaxed = relaxed
+  else
+    -- ARTn finished
+   siesta.MD.Relaxed = true
+  end --if not lconv
 
   return { "geom.xa", "MD.Relaxed" }
 
