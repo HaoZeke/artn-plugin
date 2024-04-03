03f03afa6e470dae9446478efd1e0d4f194b703d
mgoonde <miha.gunde@irb.hr>
2024-04-03 19:17:53 +0200

    add api2 file

---
+++ src/artn_api2.f90
@@ -0,0 +1,70 @@
+module artn_api2
+  use, intrinsic :: iso_c_binding
+  use precision, only: DP
+  implicit none
+
+contains
+
+  function artn_create()result( ierr )bind(C, name = "artn_create" )
+    use artn_params, only: called_from
+    use units, only: CALLER_IS_API
+    implicit none
+    integer :: ierr
+
+    ierr = 0
+
+    !! set called_from
+    called_from = CALLER_IS_API
+
+    write(*,*) "CF", called_from
+  end function artn_create
+
+
+  subroutine artn_destroy()bind(C, name = "artn_destroy" )
+  end subroutine artn_destroy
+
+
+  subroutine artn_merr( cfile, linenr )bind(C, name="artn_merr")
+    use m_error, only: merr
+    use m_tools, only: c2f_char
+    use, intrinsic :: iso_fortran_env, only: stdout => output_unit
+    implicit none
+    character(len=1, kind=c_char), intent(in) :: cfile(*)
+    integer, intent(in) :: linenr
+    character(:), allocatable :: file
+
+    allocate( file, source=c2f_char(cfile))
+    write(stdout, *) repeat("=",40)
+    write(stdout,"(a,1x,a,1x,a,1x,i0)") ">> API call to write_err called from:",trim(file),"line:",linenr
+    deallocate( file )
+    call merr( __FILE__, __LINE__, kill=.true.)
+  end subroutine artn_merr
+
+
+  !! already available routines in src
+  ! function artn_get_param_datatype( cname, ierr )result(dtype)bind(C,name="artn_get_param_datatype")
+  ! end function artn_get_param_datatype
+  ! function artn_get_param_datarank( cname, ierr ) result( drank )bind(C,name="artn_get_param_datarank")
+  ! end function artn_get_param_datarank
+  ! subroutine artn_set_param()bind(C, name="artn_set_param" )
+  ! end subroutine artn_set_param
+  ! subroutine artn_get_param()bind(C, name="artn_get_param" )
+  ! end subroutine artn_get_param
+
+
+  !! list_set
+
+  !! list_extract
+
+  !! dump_input
+
+  !! serialize
+
+  !! read_generated
+
+
+  !! get_data things.
+
+  !! get_mem things.
+
+end module artn_api2
