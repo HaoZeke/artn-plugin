4e88441f55c4d4adb37797606ad5808f316c3a98
Nsalles <nsalles33@gmail.com>
2024-05-06 21:42:57 +0200

    Replace the SUM_FORCE() by NORM2()

--- src/nextmin.f90
+++ src/nextmin.f90
@@ -80,9 +80,9 @@ SUBROUTINE save_min( nat, pos )
   Rc = unconvert_length( 0.5_DP )
 
   call compute_delr( nat, pos, tau_init, lat, delr )
-  call sum_force( delr, nat, dr1 )
+  !call sum_force( delr, nat, dr1 )
   !dr1 = dsum( 3*nat, delr ) ! Square of delr
-  !dr1 = norm2( delr )
+  dr1 = norm2( delr )
 
   !...Comparison in bohr
   if( dr1 > Rc )then
@@ -95,8 +95,9 @@ SUBROUTINE save_min( nat, pos )
      ! ...We already saved a minimum and the auestion is:
      !!   is it new/farther as the init/start position
      call compute_delr( nat, tau_nextmin, tau_init, lat, delr )
-     call sum_force( delr, nat, dr2 )
+     !call sum_force( delr, nat, dr2 )
      !dr2 = dsum( 3*nat, delr )
+     dr2 = norm2( delr )
 
      ! ...We save the minimum farther than the initial positon
      if( dr1 > dr2 ) tau_nextmin = pos
