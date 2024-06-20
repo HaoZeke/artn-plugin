6fa98875eee7bfcb3c24ce7c4d22de011f370af4
Antoine Jay <ajay@lillou>
2024-06-20 16:26:47 +0200

    Faster on 2 cores, verbos=3 to print all

--- examples/graphene.QE.d/run_example.sh
+++ examples/graphene.QE.d/run_example.sh
@@ -3,4 +3,4 @@
 #QE_PATH should be define in environment_variables
 source ../../environment_variables
 
-${PARA_PREFIX} ${QE_PATH}/bin/pw.x -partn < relax.graphene-3x2.C-vac.in | tee relax.graphene-3x2.C-vac.out
+mpirun -np 2 ${QE_PATH}/bin/pw.x -partn < relax.graphene-3x2.C-vac.in | tee relax.graphene-3x2.C-vac.out
