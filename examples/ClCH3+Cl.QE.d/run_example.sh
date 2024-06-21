1fc69366a05ba5b538378851911c4e86040f01e4
Antoine Jay <ajay@lillou>
2024-06-21 09:25:47 +0200

    Faster on 2 cores than on 4

--- examples/ClCH3+Cl.QE.d/run_example.sh
+++ examples/ClCH3+Cl.QE.d/run_example.sh
@@ -3,4 +3,4 @@
 #QE_PATH should be define in environment_variables
 source ../../environment_variables
 
-mpirun -np 4 ${QE_PATH}/bin/pw.x -partn < relax.ClCH3+Cl.in | tee relax.ClCH3+Cl.out
+mpirun -np 2 ${QE_PATH}/bin/pw.x -partn < relax.ClCH3+Cl.in | tee relax.ClCH3+Cl.out
