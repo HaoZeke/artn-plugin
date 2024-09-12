f40049d1946237114497f51e17988d2d43118f85
mgoonde <miha.gunde@gmail.com>
2024-09-12 13:30:27 +0000

    add qe_ready in run scripts of qe examples

--- examples/Li-migration.QE.d/run_example.sh
+++ examples/Li-migration.QE.d/run_example.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+QE_READY=$(grep "with_qe" ../../make.inc | cut -d "=" -f 2)
+if test $QE_READY = '"no"'; then
+	echo "pARTn has not been compiled for QE. Exiting."
+	exit
+fi
+
 QE_PATH=$(grep "QE_PATH" ../../make.inc | cut -d "=" -f 2)
 
 mpirun -np 8 ${QE_PATH}/bin/pw.x -partn < relax.Li-migration.graphite-3x2x2.in | tee relax.Li-migration.graphite-3x2x2.out 
--- examples/graphene.QE.d/run_example.sh
+++ examples/graphene.QE.d/run_example.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+QE_READY=$(grep "with_qe" ../../make.inc | cut -d "=" -f 2)
+if test $QE_READY = '"no"'; then
+	echo "pARTn has not been compiled for QE. Exiting."
+	exit
+fi
+
 QE_PATH=$(grep "QE_PATH" ../../make.inc | cut -d "=" -f 2)
 
 mpirun -np 2 ${QE_PATH}/bin/pw.x -partn < relax.graphene-3x2.C-vac.in | tee relax.graphene-3x2.C-vac.out
