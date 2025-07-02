3b3a64d4e1b841aa92802f09bacd4f28e72fbf45
Normand Mousseau <normand.mousseau@umontreal.ca>
2025-07-02 14:42:29 +0000

    Edit utils.py : added initial energy to output

--- examples/COUPLE/py_metropolis/utils.py
+++ examples/COUPLE/py_metropolis/utils.py
@@ -233,6 +233,6 @@ def create_event_list() :
 
     if os.path.exists(filename) == False:
      file = open(filename, "x")
-     file.write(" Init          Sad          Fin       Status   Accept En  Barrier      Delr sad     Delr ini     Delr fin        Random\n")
-     file.write(" **********   ************ *******  *********  *********  **********   **********   **********   **********   ***********\n")
+     file.write(" Init          Sad          Fin       Status    Init En    Init En     Barrier      Delr sad     Delr ini     Delr fin        Random\n")
+     file.write(" **********   ************ *******  *********  *********  **********  **********   **********   **********   **********   ***********\n")
      file.close()
