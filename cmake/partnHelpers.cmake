225a73e092a4a3bf10ae02d8f8a73f6095ff8225
mgoonde <miha.gunde@gmail.com>
2025-08-05 12:28:13 +0200

    move to file the configuration of QE when QE_PATH is passed

--- cmake/partnHelpers.cmake
+++ cmake/partnHelpers.cmake
@@ -1,32 +1,7 @@
 ##
-## thefunctions have prefix <artn_> just to distinguish they are custom, not from CMake
+## the functions have prefix <artn_> just to distinguish they are custom, and not from CMake.
+## There is no relation to artn.
 ##
-
-## find a string in file; return logical T/F
-function(artn_find_string in_string in_file found_str)
-  # message(STATUS "in_string: ${in_string}")
-  # message(STATUS "in_file: ${in_file}")
-
-  # # read file into array of strings
-  file(STRINGS ${in_file} this_file )
-
-  set(found False)
-
-  # parse file strings
-  while(this_file)
-    # pop an array element into LINE
-    list(POP_FRONT this_file LINE)
-    # this is a regex check if `string` matches `LINE`
-    if(${LINE} MATCHES ${in_string})
-      set(found True)
-      # message(STATUS "found line:: ${LINE}")
-      break()
-    endif()
-  endwhile()
-
-  set(found_str ${found} PARENT_SCOPE)
-
-endfunction(artn_find_string)
 
 
 ## find string in file; return whole line with string
@@ -42,7 +17,7 @@ function(artn_get_string in_string in_file out_string)
     # this is a regex check if `string` matches `LINE`
     if(${LINE} MATCHES ${in_string})
       set(found ${LINE})
-      message(STATUS "found line:: ${LINE}")
+      # message(STATUS "found line:: ${LINE}")
       break()
     endif()
   endwhile()
@@ -50,3 +25,33 @@ function(artn_get_string in_string in_file out_string)
   set(${out_string} ${found} PARENT_SCOPE)
 
 endfunction(artn_get_string)
+
+
+##
+## match version values of the format major.minor.patch
+## If any values dont exist, set them to 0, i.e. 7.5 => major=7, minor=5, patch=0
+function( artn_version_values version_str major minor patch)
+  ## version major value
+  string(REGEX MATCH "^([0-9]+)" _match "${version_str}")
+  set(${major} ${CMAKE_MATCH_1} PARENT_SCOPE)
+
+  ## version minor value
+  string(REGEX MATCH "^[0-9]+\\.([0-9]+)" _match "${version_str}")
+  if( "${CMAKE_MATCH_1}" STREQUAL "" )
+    set(mn 0)
+  else()
+    set(mn ${CMAKE_MATCH_1})
+  endif()
+
+  ## version patch value
+  string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.([0-9]+)" _match "${version_str}")
+  if( "${CMAKE_MATCH_1}" STREQUAL "" )
+    set(pt 0)
+  else()
+    set(pt ${CMAKE_MATCH_1})
+  endif()
+
+  set(${minor} ${mn} PARENT_SCOPE)
+  set(${patch} ${pt} PARENT_SCOPE)
+endfunction(artn_version_values)
+
---
+++ cmake/partn_qe_conf.cmake
@@ -0,0 +1,83 @@
+
+## this is executed when QE_PATH is passed to cmake
+
+
+##  find the root dir realtive to passed QE_PATH, by locating the 'cmake' directory
+if(IS_DIRECTORY "${QE_PATH}/../cmake" )
+  set(qeroot ${QE_PATH}/../)
+elseif(IS_DIRECTORY "${QE_PATH}/cmake" )
+  set(qeroot ${QE_PATH})
+endif()
+message(STATUS "qeroot ${qeroot}")
+
+##  find if qe is configured with make or cmake
+set(qe_make False)
+set(qe_cmake False)
+if(EXISTS "${qeroot}/make.inc")
+  set(qe_make True)
+elseif(EXISTS "${QE_PATH}/CMakeCache.txt")
+  set(qe_cmake True)
+endif()
+message(STATUS "qe_make:${qe_make} qe_cmake:${qe_cmake}")
+
+
+message(STATUS "Checking QE version")
+
+## get hard-coded version line from qe/include/qe_version.h
+artn_get_string( "version_number" ${qeroot}/include/qe_version.h line)
+
+## extract the version number which is inside ' ' or " "
+string(REGEX REPLACE "^.*['\"](.*)['\"]" "\\1" qe_version "${line}")
+message(STATUS "QE version is: ${qe_version}")
+
+artn_version_values( ${qe_version} qe_major qe_minor qe_patch )
+# message(STATUS "major:${qe_major} minor:${qe_minor} patch:${qe_patch}")
+
+## qe versions < 7.3 not supported through cmake
+if( "${qe_major}" LESS_EQUAL 7 )
+  if( "${qe_minor}" LESS 3 )
+    message(FATAL_ERROR "pARTn :: QE versions < 7.3 not supported through CMake.")
+  endif()
+endif()
+
+
+## add artn lib dependency
+if(qe_make) # for make
+
+    ## add libartn.so into make.inc, if its not there already
+    artn_get_string( "LIBOBJS \\+= ${CMAKE_BINARY_DIR}/libartn\.so" ${qeroot}/make.inc found_str)
+    if("${found_str}" STREQUAL "")
+        file(APPEND ${qeroot}/make.inc "LIBOBJS += ${CMAKE_BINARY_DIR}/libartn.so\n")
+    endif()
+
+    artn_get_string( "QELIBS \\+= ${CMAKE_BINARY_DIR}/libartn\.so" ${qeroot}/make.inc found_str)
+    if("${found_str}" STREQUAL "")
+        file(APPEND ${qeroot}/make.inc "QELIBS += ${CMAKE_BINARY_DIR}/libartn.so\n")
+    endif()
+
+    add_custom_command(TARGET artn POST_BUILD COMMAND make pw WORKING_DIRECTORY ${qeroot} COMMENT "       Building/Rebuild target pw...")
+
+elseif(qe_cmake) # for cmake
+
+    ## add dependency libartn.so into PW/CMakeLists.txt
+    artn_get_string("target_link_libraries\\(qe_pw PRIVATE ${CMAKE_BINARY_DIR}/libartn\.so\\)" ${QE_PATH}/PW/CMakeLists.txt found_str)
+    if("${found_str}" STREQUAL "")
+        file(APPEND ${QE_PATH}/PW/CMakeLists.txt "target_link_libraries(qe_pw PRIVATE ${CMAKE_BINARY_DIR}/libartn.so)\n")
+    endif()
+
+else()
+
+    ## not make and not cmake ... error
+    message(FATAL_ERROR "QE_PATH incorrect, or QE not configured neither by `make` nor `cmake`.")
+endif()
+
+## copy the plugin_ext_forces file with call to artn_QE to QE/PW/src
+file(COPY_FILE ${CMAKE_CURRENT_SOURCE_DIR}/Files_QE/plugin_ext_forces.f90 ${qeroot}/PW/src/plugin_ext_forces.f90)
+
+## not sure what this part does
+if(qe_cmake)
+    add_custom_command(TARGET artn POST_BUILD COMMAND cmake --build . --target pw WORKING_DIRECTORY ${QE_PATH} COMMENT "       Building/Rebuild target pw...")
+elseif(NOT qe_make)
+    message(WARNING "Either set QE_PATH to the cmake build directory (e.g. -DQE_PATH=/path/to/QE/build), or rebuild QE manualy after building this project")
+endif()
+
