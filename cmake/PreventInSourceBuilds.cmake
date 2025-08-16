bd2b0568e445e2a15f34e6f6ee1ffa107bea5b2e
Eugène Sanscartier <eugene.sanscartier@gmail.com>
2025-08-16 11:17:14 +0800

    cmake formatting

--- cmake/PreventInSourceBuilds.cmake
+++ cmake/PreventInSourceBuilds.cmake
@@ -2,18 +2,19 @@
 # https://stackoverflow.com/questions/1208681/with-cmake-how-would-you-disable-in-source-builds/
 
 function(prevent_in_source_builds)
-  get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
-  get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
+    get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
+    get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
 
-  if("${srcdir}" STREQUAL "${bindir}")
-    message(FATAL_ERROR
-      "\nIn-source builds are not allowed!\n"
-      "Please create a separate build directory, e.g.:\n"
-      "  mkdir build && cd build && cmake ..\n"
-      "To clean up after this aborted in-place compilation:\n"
-      "  rm -rf CMakeCache.txt CMakeFiles\n"
-    )
-  endif()
+    if("${srcdir}" STREQUAL "${bindir}")
+        message(
+            FATAL_ERROR
+            "\nIn-source builds are not allowed!\n"
+            "Please create a separate build directory, e.g.:\n"
+            "  mkdir build && cd build && cmake ..\n"
+            "To clean up after this aborted in-place compilation:\n"
+            "  rm -rf CMakeCache.txt CMakeFiles\n"
+        )
+    endif()
 endfunction()
 
 prevent_in_source_builds()
--- cmake/partnHelpers.cmake
+++ cmake/partnHelpers.cmake
@@ -3,55 +3,51 @@
 ## There is no relation to artn.
 ##
 
-
 ## find string in file; return whole line with string
 function(artn_get_string in_string in_file out_string)
-  # # read file into array of strings
-  file(STRINGS ${in_file} this_file )
+    # # read file into array of strings
+    file(STRINGS ${in_file} this_file)
 
-  set(found "")
-  # parse file strings
-  while(this_file)
-    # pop an array element into LINE
-    list(POP_FRONT this_file LINE)
-    # this is a regex check if `string` matches `LINE`
-    if(${LINE} MATCHES ${in_string})
-      set(found ${LINE})
-      # message(STATUS "found line:: ${LINE}")
-      break()
-    endif()
-  endwhile()
+    set(found "")
+    # parse file strings
+    while(this_file)
+        # pop an array element into LINE
+        list(POP_FRONT this_file LINE)
+        # this is a regex check if `string` matches `LINE`
+        if(${LINE} MATCHES ${in_string})
+            set(found ${LINE})
+            # message(STATUS "found line:: ${LINE}")
+            break()
+        endif()
+    endwhile()
 
-  set(${out_string} ${found} PARENT_SCOPE)
-
+    set(${out_string} ${found} PARENT_SCOPE)
 endfunction(artn_get_string)
-
 
 ##
 ## match version values of the format major.minor.patch
 ## If any values dont exist, set them to 0, i.e. 7.5 => major=7, minor=5, patch=0
-function( artn_version_values version_str major minor patch)
-  ## version major value
-  string(REGEX MATCH "^([0-9]+)" _match "${version_str}")
-  set(${major} ${CMAKE_MATCH_1} PARENT_SCOPE)
+function(artn_version_values version_str major minor patch)
+    ## version major value
+    string(REGEX MATCH "^([0-9]+)" _match "${version_str}")
+    set(${major} ${CMAKE_MATCH_1} PARENT_SCOPE)
 
-  ## version minor value
-  string(REGEX MATCH "^[0-9]+\\.([0-9]+)" _match "${version_str}")
-  if( "${CMAKE_MATCH_1}" STREQUAL "" )
-    set(mn 0)
-  else()
-    set(mn ${CMAKE_MATCH_1})
-  endif()
+    ## version minor value
+    string(REGEX MATCH "^[0-9]+\\.([0-9]+)" _match "${version_str}")
+    if("${CMAKE_MATCH_1}" STREQUAL "")
+        set(mn 0)
+    else()
+        set(mn ${CMAKE_MATCH_1})
+    endif()
 
-  ## version patch value
-  string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.([0-9]+)" _match "${version_str}")
-  if( "${CMAKE_MATCH_1}" STREQUAL "" )
-    set(pt 0)
-  else()
-    set(pt ${CMAKE_MATCH_1})
-  endif()
+    ## version patch value
+    string(REGEX MATCH "^[0-9]+\\.[0-9]+\\.([0-9]+)" _match "${version_str}")
+    if("${CMAKE_MATCH_1}" STREQUAL "")
+        set(pt 0)
+    else()
+        set(pt ${CMAKE_MATCH_1})
+    endif()
 
-  set(${minor} ${mn} PARENT_SCOPE)
-  set(${patch} ${pt} PARENT_SCOPE)
+    set(${minor} ${mn} PARENT_SCOPE)
+    set(${patch} ${pt} PARENT_SCOPE)
 endfunction(artn_version_values)
-
